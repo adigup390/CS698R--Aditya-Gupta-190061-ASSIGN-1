1912cbe70fc7ae1ae1ad8d76e38fc061f7d88678
adigup390 <65325056+adigup390@users.noreply.github.com>
2021-09-11 20:58:00 +0530

    Delete setup.py

--- Gym Envs/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='gym_env',  # Use same name as sibling directory.
-    version='1.0.0',  # Version number.
-    install_requires=['gym']  # Python packages required by our environments.
-)
